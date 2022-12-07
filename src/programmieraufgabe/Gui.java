a30e7678d0d0d3d3e4be009188a20de60dbefe5c
Lukas248 <lukas.steegmaier@gmail.com>
2022-12-07 12:24:41 +0100

    Initial commit

---
+++ src/programmieraufgabe/Gui.java
@@ -0,0 +1,13 @@
+package programmieraufgabe;
+
+import javax.swing.*;
+
+public class Gui extends JFrame {
+
+    public Gui() {
+
+        setSize(400,400);
+
+    }
+
+}
---
+++ src/programmieraufgabe/Main.java
@@ -0,0 +1,12 @@
+package programmieraufgabe;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Gui gui = new Gui();
+        gui.setVisible(true);
+
+    }
+
+}
